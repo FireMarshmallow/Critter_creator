45faf461f4d744d2c1baf6072cfa50f9276063f2
Mellow <62353091+FireMarshmallow@users.noreply.github.com>
2021-10-14 05:48:30 +0100

    maintenance 14/10/21 Still working

--- Critter_Creator.py
+++ Critter_Creator.py
@@ -1,13 +1,23 @@
+"""
+Code by
+  __  __      _ _                 _____ _          
+ |  \/  | ___| | | _____      __ |  ___(_)_ __ ___ 
+ | |\/| |/ _ \ | |/ _ \ \ /\ / / | |_  | | '__/ _ \
+ | |  | |  __/ | | (_) \ V  V /  |  _| | | | |  __/
+ |_|  |_|\___|_|_|\___/ \_/\_/   |_|   |_|_|  \___|
+check out my stuff on https://linktr.ee/Mellowfire
+"""
 import requests
 from bs4 import BeautifulSoup
 import random
 
-url = 'https://a-z-animals.com/animals/'
+url = "https://a-z-animals.com/animals/"
 global headers
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
+}
 request = requests.get(url, headers=headers)
-soup = BeautifulSoup(request.text, 'html.parser')
+soup = BeautifulSoup(request.text, "html.parser")
 animals = soup.find_all(class_="col-md-4")
 global imagess
 global links
@@ -24,16 +34,16 @@ def run():
     for _ in range(9):
         randompicks = random.choices(animals)
         for ras in randompicks:
-            link = ras.find('a')['href']
+            link = ras.find("a")["href"]
             name = ras.text
-            link2 = link+"#single-animal-text"
+            link2 = link + "#single-animal-text"
             request2 = requests.get(link2, headers=headers)
-            bs = BeautifulSoup(request2.text, 'html.parser')
+            bs = BeautifulSoup(request2.text, "html.parser")
             images = bs.findAll(class_="center-block")
             # ing_fill_2 = images.findall("img", {"src": True})
             names.append(name)
             links.append(link)
-            imagess.append(images[2].attrs['data-lazy'])
+            imagess.append(images[2].attrs["data-lazy"])
             break
 
     if len(names) != 9 or len(links) != 9 or len(imagess) != 9:
